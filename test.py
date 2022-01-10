9dd62b49c9e87542ab53307637cd102afc32f747
Jakhon <jahongirsdv@gmail.com>
2022-01-10 21:52:12 +0500

    for heroku

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 
-from re import T
 from typing import Text
 import telebot
 from telebot import types
@@ -143,7 +142,7 @@ def process_upload(message,nomer):
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
     # определяем путь загрузки с именем файла
-        src = 'C:/Users/fuji/Documents/GitHub/botxat/files/' + message.document.file_name
+        src = '../files/' + message.document.file_name
     # открываем файл для записи
         with open(src, 'wb') as new_file:
         # записываем данные в файл
@@ -163,7 +162,7 @@ def process_upload_uz(message,nomer):
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
     # определяем путь загрузки с именем файла
-        src = 'C:/Users/fuji/Documents/GitHub/botxat/files/' + message.document.file_name
+        src = 'C:/Users/fuji/db/files/' + message.document.file_name
     # открываем файл для записи
         with open(src, 'wb') as new_file:
         # записываем данные в файл
@@ -352,4 +351,4 @@ def process_theme_step(message):
 
 
 
-bot.polling(skip_pending=True)
+bot.polling()
